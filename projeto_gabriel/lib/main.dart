e3d73d90946c814636ae3a54ec2130f0af0cc0da
alunos <alunos@IFRSL.redelocal>
2026-05-13 15:27:28 -0300

    Criada a estilização e a exibição de dados

--- projeto_gabriel/lib/main.dart
+++ projeto_gabriel/lib/main.dart
@@ -54,4 +54,52 @@ class _HomePageState extends State<HomePage> {
     setState(() { 
       transactions.add(custo); });
   }
-  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Controle Financeiro"),
+        centerTitle: true,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                children: [
+
+                  const Text(
+                    "Total de Gastos",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Text(
+                    "R\$ ${total.toStringAsFixed(2)}",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 20),
+          ]
+        )));
+      }}
